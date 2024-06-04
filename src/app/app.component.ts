a897ff4d5c8811f157ad81ebd91f296899ff6c05
ethika-snolan <snolan@ethika.com>
2024-06-04 15:08:21 -0700

    - updated editorconfig  - formatting changes  - added utc time test for mismatch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,24 +2,26 @@ import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { ThemeToggleComponent } from './theme-toggle/theme-toggle.component'
 import { ThemeService } from './theme.service'
+import { NgIf } from '@angular/common'
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, ThemeToggleComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, ThemeToggleComponent, NgIf],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'matter-ng'
+	title = 'matter-ng'
+	dt: string = new Date().toUTCString()
 
-  constructor(private themeService: ThemeService) {
-    if(typeof window !== 'undefined') {
-      const localDarkMode = localStorage.getItem('dark-mode')
-      if (localDarkMode) {
-        const theMode = JSON.parse(localDarkMode)
-        this.themeService.setDarkMode(theMode)
-      }
-    }
-  }
+	constructor(private themeService: ThemeService) {
+		if (typeof window !== 'undefined') {
+			const localDarkMode = localStorage.getItem('dark-mode')
+			if (localDarkMode) {
+				const theMode = JSON.parse(localDarkMode)
+				this.themeService.setDarkMode(theMode)
+			}
+		}
+	}
 }
